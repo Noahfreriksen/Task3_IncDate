6a846700623d678a855297e049b152ff5ef84947
noahfreriksen <noah_freriksen@hotmail.com>
2019-01-21 14:41:38 +0100

    changed 3 tests to 1 main test

--- tests/date_tests.cpp
+++ tests/date_tests.cpp
@@ -3,26 +3,13 @@
 #include "../lib/date.cpp"
 
 /*
- * Test bench that testes the 3 functions of the date class
+ * Test bench that testes the main function of the date class
  */
-
-TEST(date_tests, ConstructorTest){
-    time_t now = time(nullptr);
-    date d1;
-    ASSERT_EQ(now, d1.now);
-}
-
-TEST(date_tests, IncTest){
-    time_t t1 = 386400;
-    date d1;
-    d1.now = 300000;
-    d1.inc(1);
-    ASSERT_EQ(t1, d1.now);
-}
 
 TEST(date_tests, ToString){
     date d1;
     d1.now = 30;
-    ASSERT_EQ("1-1-1970", d1.toString());
+    d1.inc(2);
+    ASSERT_EQ("3-1-1970", d1.toString());
 }
 
